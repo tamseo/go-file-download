4102d4bc4ef36c86bce684a99abe2abc9a8cc668
Tam Nguyen <tamseo@gmail.com>
2017-12-25 14:26:12 +0900

    Improve implementation

--- main.go
+++ main.go
@@ -7,18 +7,15 @@ import (
 	"encoding/hex"
 	"fmt"
 	"github.com/cavaliercoder/grab"
+	"time"
 )
 
 func main() {
 	const md5Original = "cd573cfaace07e7949bc0c46028904ff"
 	url := "http://90.130.70.73/1GB.zip"
 	fmt.Printf("Downloading %s...\n", url)
-	resp, err := grab.Get(".", url)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error downloading %s: %v\n", url, err)
-		os.Exit(1)
-	}
 
+	resp := downLoad(url);
 	fmt.Printf("File size %d \n", resp.Size)
 
 	file, _ := os.Open(resp.Filename)
@@ -46,4 +43,43 @@ func getMD5Hash(file *os.File) (string, error) {
 	md5String = hex.EncodeToString(hashInBytes)
 	return md5String, nil
 
+}
+
+func downLoad(url string) *grab.Response {
+	// create client
+	client := grab.NewClient()
+	req, _ := grab.NewRequest(".", url)
+
+	// start download
+	fmt.Printf("Downloading %v...\n", req.URL())
+	resp := client.Do(req)
+	fmt.Printf("  %v\n", resp.HTTPResponse.Status)
+
+	// start UI loop
+	t := time.NewTicker(500 * time.Millisecond)
+	defer t.Stop()
+
+Loop:
+	for {
+		select {
+		case <-t.C:
+			fmt.Printf("  transferred %v / %v bytes (%.2f%%)\n",
+				resp.BytesComplete(),
+				resp.Size,
+				100*resp.Progress())
+
+		case <-resp.Done:
+			// download is complete
+			break Loop
+		}
+	}
+
+	// check for errors
+	if err := resp.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "Download failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Download saved to ./%v \n", resp.Filename)
+	return resp
 }
